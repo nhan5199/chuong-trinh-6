0e3ecc6c309d2be9b296e1ce09eb3528bbe7a677
NhanN <NhanN@fpt.com.vn>
2023-11-16 01:53:50 +0700

    fix responsive

--- src/app/index/gioi-thieu-chung/gioi-thieu-chung-video/gioi-thieu-chung-video.component.ts
+++ src/app/index/gioi-thieu-chung/gioi-thieu-chung-video/gioi-thieu-chung-video.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gioi-thieu-chung-video',
   templateUrl: './gioi-thieu-chung-video.component.html',
-  styleUrls: ['./gioi-thieu-chung-video.component.css']
+  styleUrls: ['./gioi-thieu-chung-video.component.css'],
 })
 export class GioiThieuChungVideoComponent implements OnInit {
+  constructor(private readonly _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClickNavigate() {
+    this._router.navigateByUrl('/gioithieuchung');
   }
-
 }
--- src/app/index/gioi-thieu-chung/gioi-thieu-chung.component.ts
+++ src/app/index/gioi-thieu-chung/gioi-thieu-chung.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gioi-thieu-chung',
   templateUrl: './gioi-thieu-chung.component.html',
-  styleUrls: ['./gioi-thieu-chung.component.css']
+  styleUrls: ['./gioi-thieu-chung.component.css'],
 })
 export class GioiThieuChungComponent implements OnInit {
+  constructor(private readonly _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClickNavigate(url: string) {
+    this._router.navigateByUrl(url);
   }
-
 }
